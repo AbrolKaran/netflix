896870e82354b8280f663f7db13be0ac3d6f2e64
rsus4 <96760386+rishit31@users.noreply.github.com>
2022-12-12 13:09:40 +0530

    URL Routing and Exception Handling

--- src/playlists/views.py
+++ src/playlists/views.py
@@ -1,8 +1,11 @@
 from re import template
-from django.views.generic import ListView
-
+from telnetlib import DET
+from django.http import Http404
+from django.views.generic import ListView, DeleteView
+from django.utils import timezone
+from neflix.db.models import PublishStateOptions
 # Create your views here.
-from .models import Playlist, MovieProxy, TVShowProxy
+from .models import Playlist, MovieProxy, TVShowProxy, TVShowSeasonProxy
 
 class PlaylistMixin():
     template_name = 'playlist_list.html'
@@ -12,6 +15,8 @@ class PlaylistMixin():
         context = super().get_context_data(* args, **kwargs)
         if self.title is not None:
             context['title'] = self.title
+        
+        print(context)
         return context
 
     def get_queryset(self):
@@ -22,6 +27,38 @@ class MovieListView(PlaylistMixin, ListView):
     queryset = MovieProxy.objects.all()
     title = "Movies"
 
+class MovieDetailView(PlaylistMixin, DeleteView):
+    template_name = 'playlists/movie_detail.html'
+    queryset = MovieProxy.objects.all()
+
+class PlaylistDetailView(PlaylistMixin, DeleteView):
+    template_name = 'playlists/playlist_detail.html'
+    queryset = Playlist.objects.all()
+
+
+class TVShowDetailView(PlaylistMixin, DeleteView):
+    template_name = 'playlists/tvshow_detail.html'
+    queryset = TVShowProxy.objects.all()
+
+
+class TVShowSeasonDetailView(PlaylistMixin, DeleteView):
+    template_name = 'playlists/season_detail.html'
+    queryset = TVShowSeasonProxy.objects.all()
+
+    def get_object(self):
+        kwargs = self.kwargs
+        show_slug = kwargs.get("showSlug")
+        season_slug = kwargs.get("seasonSlug")
+        now = timezone.now()
+        try:
+            obj = TVShowSeasonProxy.objects.get(state =PublishStateOptions.PUBLISH, publish_timestamp__lte=now,parent__slug_iexact = show_slug, slug__iexact =season_slug)
+        except TVShowSeasonProxy.MultipleObjectsReturned:
+            qs = TVShowSeasonProxy.objects.filter(parent__slug_iexact = show_slug, slug__iexact =season_slug).published()
+            obj = qs.first()
+            # log this
+        except:
+            raise Http404
+        return obj
 
 class TVShowListView(PlaylistMixin, ListView):
     queryset= TVShowProxy.objects.all()
